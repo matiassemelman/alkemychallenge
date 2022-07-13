0502bf73695a5bc36efaa291ce95e6d59bcd2e90
matiassemelman <matias.semelman@protonmail.com>
2022-07-13 13:38:23 -0600

    First commit

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+export const Login = () => {
+  return (
+    <form>
+        <h2>Log In</h2>
+    <label for="email">Email 
+    <br />
+        <input type="email" name="email" id="email" />
+    </label>
+<br />
+    <label for="password">Password 
+    <br />
+        <input type="password" name="password" id="password" />
+    </label>
+
+    </form>
+  )
+}
+
