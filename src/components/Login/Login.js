8cbc13551de085a492d14d73e835b587765bf622
matiassemelman <matias.semelman@protonmail.com>
2022-07-14 13:48:30 -0600

    Redirect from Login implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,58 +1,35 @@
+// Components
+
 import { Header, Form, Label, Input, Button } from "./styled";
+
+// Libraries
 import axios from "axios";
 import Swal from "sweetalert2";
 import "animate.css";
 
-const submitHandler = (e) => {
-  e.preventDefault();
-  const email = e.target.email.value;
-  const password = e.target.password.value;
+// Hooks
+import { useNavigate  } from "react-router-dom";
 
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const Login = () => {
+  const navigate = useNavigate();
 
-  if (email === "" || password === "") {
-    Swal.fire({
-      icon: "error",
-      timer: 2000,
-      timerProgressBar: true,
-      title: "Fields cannot be empty",
-      showClass: {
-        popup: "animate__animated animate__fadeInDown",
-      },
-      hideClass: {
-        popup: "animate__animated animate__fadeOutDown",
-      },
-    });
-  }
+  const submitHandler = (e) => {
+  
 
-  if (email !== "" && !emailRegex.test(email)) {
-    Swal.fire({
-      icon: "error",
-      timer: 2000,
-      timerProgressBar: true,
-      title: "Invalid email format",
-      showClass: {
-        popup: "animate__animated animate__fadeInDown",
-      },
-      hideClass: {
-        popup: "animate__animated animate__fadeOutDown",
-      },
-    });
-  }
-  if (email !== "challenge@alkemy.org" && password !== "react") {
-    console.log("Wrong credentials");
-  }
-  axios
-    .post("http://challenge-react.alkemy.org", { email, password })
-    .then((res) => {
-      const token = res.data.token;
-      localStorage.setItem('token', token)
+
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+  
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    if (email === "" || password === "") {
       Swal.fire({
-        icon: "success",
+        icon: "error",
         timer: 2000,
         timerProgressBar: true,
-        title: `Logged in : ${token}`,
+        title: "Fields cannot be empty",
         showClass: {
           popup: "animate__animated animate__fadeInDown",
         },
@@ -60,10 +37,50 @@ const submitHandler = (e) => {
           popup: "animate__animated animate__fadeOutDown",
         },
       });
-    });
-};
+    }
+  
+    if (email !== "" && !emailRegex.test(email)) {
+      Swal.fire({
+        icon: "error",
+        timer: 2000,
+        timerProgressBar: true,
+        title: "Invalid email format",
+        showClass: {
+          popup: "animate__animated animate__fadeInDown",
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutDown",
+        },
+      });
+    }
+    if (email !== "challenge@alkemy.org" && password !== "react") {
+      console.log("Wrong credentials");
+    }
+    axios
+      .post("http://challenge-react.alkemy.org", { email, password })
+      .then((res) => {
+        const token = res.data.token;
+        localStorage.setItem('token', token)
+        Swal.fire({
+          icon: "success",
+          timer: 2000,
+          timerProgressBar: true,
+          title: `Logged in : ${token}`,
+          showClass: {
+            popup: "animate__animated animate__fadeInDown",
+          },
+          hideClass: {
+            popup: "animate__animated animate__fadeOutDown",
+          },
+        });
+        navigate("/list", { replace: true });
+      });
+  };
 
-export const Login = () => {
+
+
+
+  // RENDER ====================================================
   return (
     <>
       <Header>Log In</Header>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+// Libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
+
+// Components
 import { Login } from "./components/Login/Login";
+import MovieList from './components/MovieList/MovieList'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +15,8 @@ root.render(
 
     <Routes>
       <Route path="/" exact element={<Login />} />
+      <Route path="/list" exact element={<MovieList />} />
+
     </Routes>
   </BrowserRouter>
 );
