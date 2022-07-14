fb29048ca65cbeb00b9b691f66660f7fea186657
matiassemelman <matias.semelman@protonmail.com>
2022-07-14 12:35:04 -0600

    Axios and Alerts Implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,36 +1,66 @@
 import { Header, Form, Label, Input, Button } from "./styled";
 import axios from "axios";
+import Swal from "sweetalert2";
+import "animate.css";
 
 const submitHandler = (e) => {
   e.preventDefault();
-  const email = e.target.email.value
-  const password = e.target.password.value
-  
-  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (email === "" || password === "") {
-    
-    console.log('Fields cannot be empty')
+    Swal.fire({
+      icon: "error",
+      timer: 2000,
+      timerProgressBar: true,
+      title: "Fields cannot be empty",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutDown",
+      },
+    });
   }
 
   if (email !== "" && !emailRegex.test(email)) {
-    console.log('Invalid email format')
-    
+    Swal.fire({
+      icon: "error",
+      timer: 2000,
+      timerProgressBar: true,
+      title: "Invalid email format",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutDown",
+      },
+    });
   }
   if (email !== "challenge@alkemy.org" && password !== "react") {
-    console.log('Wrong credentials')
+    console.log("Wrong credentials");
   }
-  console.log('Logged in')
-  axios.post('http://challenge-react.alkemy.org', {email, password})
-  .then(res => {
-    const token = res.data.token
-    console.log(token)
-  } 
-  )
-
-
-
-}
+  axios
+    .post("http://challenge-react.alkemy.org", { email, password })
+    .then((res) => {
+      const token = res.data.token;
+      Swal.fire({
+        icon: "success",
+        timer: 2000,
+        timerProgressBar: true,
+        title: `Logged in : ${token}`,
+        showClass: {
+          popup: "animate__animated animate__fadeInDown",
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutDown",
+        },
+      });
+    });
+};
 
 export const Login = () => {
   return (
