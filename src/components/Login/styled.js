c0ff481adb3e4b0ad35341b868c7b2166f3f76b5
matiassemelman <matias.semelman@protonmail.com>
2022-07-14 11:25:23 -0600

    Login page - Styled Components created

---
+++ src/components/Login/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled('h2')``
+
+export const Form = styled('form')``
+
+export const Label = styled('label')``
+
+export const Input = styled('input')``
+
+export const Button = styled('button')``
+
+
+
