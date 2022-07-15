d359d5d68d443cb26db8160eb1b245c27b332c8b
matiassemelman <matias.semelman@protonmail.com>
2022-07-15 14:00:44 -0600

    Order the main layout

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
 
 // Components
+import Footer from "./components/Footer/Footer";
 import { Login } from "./components/Login/Login";
-import MovieList from './components/MovieList/MovieList'
+import MovieList from "./components/MovieList/MovieList";
+import { Header } from './components/Login/styled';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <GlobalStyle />
-
+    <Header />
     <Routes>
       <Route path="/" exact element={<Login />} />
-      <Route path="/list" exact element={<MovieList />} />
-
+      <Route path="/list" element={<MovieList />} />
     </Routes>
+    {/* <Footer /> */}
+
   </BrowserRouter>
 );
