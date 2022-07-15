1e8433dbfa6e80a5719debcc3bbd177513aa5e0e
matiassemelman <matias.semelman@protonmail.com>
2022-07-15 14:01:26 -0600

    Footer styled components created

---
+++ src/components/Footer/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Footer = styled('footer')``
+
+export const Nav = styled('nav')``
+
+export const Ul = styled('ul')``
+
+export const Li = styled('li')``
+
+export const A = styled('a')``
+
+export const P = styled('p')``
+
+
