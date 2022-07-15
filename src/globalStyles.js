b01e6bc669faa277e009e85e84a4e441d48a5744
matiassemelman <matias.semelman@protonmail.com>
2022-07-15 14:00:30 -0600

    Background Image

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: #0F2027; 
+    background-image: url('https://cdn3.f-cdn.com/contestentries/1489974/18545046/5cba2aba229c4_thumb900.jpg'); 
     color: #FFFFFF;
     font-family: Open-Sans, Helvetica, Sans-Serif;
     width: 100%;
